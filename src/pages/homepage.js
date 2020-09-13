b6d0c3e4e0283cbb451c0f50039129b9063a1af8
enermis <fulgens.ailurus@gmail.com>
2020-09-13 11:07:04 +0200

    Flesh out homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { BasicLayout } from '../components/layout';
+import { Jumbotron, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
     return (
         <BasicLayout>
-            Welcome
+            <Jumbotron>
+                <h1>Hello, world!</h1>
+                <p>
+                    This is a simple hero unit, a simple jumbotron-style component for calling
+                    extra attention to featured content or information.
+                </p>
+                <p>
+                    <Link to="/play">
+                        <Button variant="primary">Start Playing Now!</Button>
+                    </Link>
+                </p>
+            </Jumbotron>
         </BasicLayout>
     )
 }
