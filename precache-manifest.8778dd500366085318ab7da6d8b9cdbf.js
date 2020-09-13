ecb99f79f01109952ece4ded536b3d86da14be1f
enermis <fulgens.ailurus@gmail.com>
2020-09-13 14:21:32 +0200

    Updates

---
+++ precache-manifest.8778dd500366085318ab7da6d8b9cdbf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5fc74c0af3a2b4799165d79c5bb1f2",
+    "url": "/whatzrface/index.html"
+  },
+  {
+    "revision": "46b2017b05b8303302bd",
+    "url": "/whatzrface/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "30afecea011331926e15",
+    "url": "/whatzrface/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46b2017b05b8303302bd",
+    "url": "/whatzrface/static/js/2.ec13f0ba.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/whatzrface/static/js/2.ec13f0ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30afecea011331926e15",
+    "url": "/whatzrface/static/js/main.98a55d6e.chunk.js"
+  },
+  {
+    "revision": "5ef2d9bfc5accd006091",
+    "url": "/whatzrface/static/js/runtime-main.cc91f313.js"
+  },
+  {
+    "revision": "3d65446bd138aa43d1354c8bfdf7f83f",
+    "url": "/whatzrface/static/media/barack-obama.3d65446b.jpg"
+  },
+  {
+    "revision": "9a77ac294105baf75d86a7562ddad794",
+    "url": "/whatzrface/static/media/dummyPerson.9a77ac29.jpg"
+  },
+  {
+    "revision": "3c3f6866edb7f7f66c860782348409f9",
+    "url": "/whatzrface/static/media/elon-musk.3c3f6866.jpg"
+  },
+  {
+    "revision": "a4112ba33dc122fe56ec23a021551db4",
+    "url": "/whatzrface/static/media/marie-curie.a4112ba3.jpg"
+  },
+  {
+    "revision": "b036345da878e48ef018a9649e15d0b0",
+    "url": "/whatzrface/static/media/robert-pattinson.b036345d.jpg"
+  },
+  {
+    "revision": "dc1b1c11b25260388ae3b5794e813e4d",
+    "url": "/whatzrface/static/media/wolfgang-amadeus-mozart.dc1b1c11.jpg"
+  }
+]);
